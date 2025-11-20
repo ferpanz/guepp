66b64888b762179739d36f37e75788b68e3bf6c3
Fernán <ferpanz@gmail.com>
2025-11-20 10:13:37 -0300

    footer mejorado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/guepp/Logo-sin-fondo.png'
 
 
 const Footer = () => {
   return (
-    <>
-        
-        
+    <footer className="position-fixed bottom-0 start-0 w-100">
+      <Link to="/" aria-label="Ir a Guepp" style={{ textDecoration: 'none', color: 'inherit' }}>
         <div className="container text-center rounded-3">
-            <hr className='w-100'/>
-            <img className='img-fluid w-25' src={logo} alt="" />  
-        </div> 
-    </>
+          <hr className="w-100" />
+          <img className="img-fluid w-25" src={logo} alt="Guepp" />
+        </div>
+      </Link>
+    </footer>
   )
 }
 
