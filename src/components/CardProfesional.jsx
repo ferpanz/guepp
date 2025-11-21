12f2e45b947de98222bf3d2b5931b7fc61c3e9b3
Fernán <ferpanz@gmail.com>
2025-11-20 23:20:10 -0300

    listo listo

--- src/components/CardProfesional.jsx
+++ src/components/CardProfesional.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import Img1 from '../assets/guepp/foto-tienda-ropa.png'
-import Img2 from '../assets/guepp/foto-pasteleria.png'
-import Img3 from '../assets/guepp/foto-almacen.png'
+import video from '../assets/guepp/videito-prof.webm'
 import Carrusel from '../subcomponents/Carrusel'
 
 const CardProfesional = () => {
   return (
     <>
-      <div className="container bg-turqueza p-5 rounded-3 mb-3">
+      <div className="container p-5 rounded-3 mb-3 bg-turqueza">
           <div className="row">
-            <div className="col-12 col-md-6 my-auto mx-auto">
-              <Carrusel images={[Img1, Img2, Img3]} />
+            <div className="col-12 col-md-6 my-auto mx-auto text-center">
+              <video className='img-fluid rounded w-50' src={video} autoPlay loop />
             </div>
             <div className="col-12 col-md-6 ps-md-5 mx-auto my-auto pt-3">
               <h2 className='fw-bold'>GÜEPP! Profesionales</h2>
@@ -18,7 +16,6 @@ const CardProfesional = () => {
               <p className='h5'>Linkeá tus redes sociales, medios de contacto y más.</p>
               <hr />
               <p className='fw-bold'>GÜEPP! CARD + QR Imprimible <br/>Precio promocional: $35000</p>
-                      
             </div>
           </div>
       </div>
